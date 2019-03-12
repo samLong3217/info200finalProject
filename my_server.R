495d9546aeba8a079429b4cebeb281760f8b50ac
PooCluster <poocluster@gmail.com>
2019-03-12 15:29:35 -0700

    Added text analysis.

--- my_server.R
+++ my_server.R
@@ -3,8 +3,9 @@ library("ggplot2")
 source("test_scores_schools_mark.R")
 
 my_server <- function(input, output) {
+  plot_copy <- NULL # won't be null after renderPlot
+  
   output$line_graph <- renderPlot({
-    print("accessed")
     tests <- c()
     if (input$act) {
       tests <- c(tests, "ACT.Composite.75th.percentile.score")
@@ -15,7 +16,6 @@ my_server <- function(input, output) {
     if (input$sat_read) {
       tests <- c(tests, "SAT.Critical.Reading.75th.percentile.score")
     }
-    print(tests)
     # selected at least one of the tests
     if (input$act | input$sat_math | input$sat_read) {
       plot <- avg_test_scores_by_interval_graph(100, tests)
@@ -27,6 +27,31 @@ my_server <- function(input, output) {
         "SAT.Critical.Reading.75th.percentile.score"
       ))
     }
+    plot_copy <- plot
     plot
+  })
+  
+  output$test_scores_text <- renderText({
+    text <- ""
+    if (input$act) {
+      avg <- round(mean(plot_copy$ACT.Composite.75th.percentile.score, na.rm = FALSE), digits = 0)
+      text <- paste0(text, "Average ACT Composite: ", avg, ". ")
+    }
+    if (input$sat_math) {
+      avg <- round(mean(plot_copy$SAT.Math.75th.percentile.score, na.rm = FALSE), digits = 0)
+      text <- paste0(text, "Average SAT Math: ", avg, ". ")
+    }
+    if (input$sat_read) {
+      avg <- round(mean(plot_copy$SAT.Critical.Reading.75th.percentile.score, na.rm = FALSE), digits = 0)
+      text <- paste0(text, "Average SAT Critical Reading: ", avg, ". ")
+    }
+    text <- paste0(text, "Across all three tests, the line graph consistently shows a downward trend where ")
+    text <- paste0(text, "college admission rate (independent variable) has an inverse relationship with ")
+    text <- paste0(text, "the corresponding test scores. Also, notice when the admission rate is lower (~ 0% - 50%), ")
+    text <- paste0(text, "the scores fluctuate a lot more than when the admission rate is higher. However, if we took ")
+    text <- paste0(text, "the average of the fluctuating slope of any of the test scores would be negative, meaning ")
+    text <- paste0(text, "that the holistic progress of the graph is that test scores decrease as admission rates ")
+    text <- paste0(text, "increase. In the reality of things, this basically means that the more competitive schools ")
+    text <- paste0(text, "are (lower admissions rate), the higher the scores are, which answers our question.")
   })
 }
--- my_ui.R
+++ my_ui.R
@@ -13,19 +13,22 @@ page_two <- tabPanel( #Cameron's Question goes here
   p("bla")
 )
 
-page_three <- tabPanel( #Marks's Question goes here
+page_three <- tabPanel( # Marks's Question goes here
   "3",
   titlePanel("Are highly selective schools also schools with high test scores?"),
   sidebarLayout(
     sidebarPanel(
+      h5(a("Which Tests Do You Want To View?")),
       checkboxInput(inputId = "act", "ACT Composite", FALSE),
       checkboxInput(inputId = "sat_math", "SAT Math", FALSE),
-      checkboxInput(inputId = "sat_read", "SAT Reading", FALSE)
+      checkboxInput(inputId = "sat_read", "SAT Reading", FALSE),
+      sliderInput(inputId = "divisions", "How Many Plots Per Test?", min = 0, max = 100, value = 10)
     ),
     mainPanel(
       plotOutput(outputId = "line_graph")
     )
-  )
+  ),
+  textOutput(outputId = "test_scores_text")
 )
 
 page_four <- tabPanel( #Rory's Question goes here
